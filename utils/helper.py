c160cdad5e508921274ec51c34e2fc1c31576b69
heyu1145 <heyu12366@outlook.com>
2025-09-06 21:25:32 +0800

    Update helper.py

--- utils/helper.py
+++ utils/helper.py
@@ -2,13 +2,13 @@ def get_all_commands(bot):
     """Return list of command objects"""
     return list(bot.tree.get_commands())
 
-   def get_command_info(bot, cmd: str):
-       """Return dict with command info or None if not found"""
-       for command in bot.tree.get_commands():
-           if command.name == cmd:
-               return {
-                "name": command.name,
-                "description": command.description,
-                "options": command.options
-            }
-            return None
+def get_command_info(bot, cmd: str):
+    """Return dict with command info or None if not found"""
+    for command in bot.tree.get_commands():
+        if command.name == cmd:
+            return {
+             "name": command.name,
+             "description": command.description,
+             "options": command.options
+         }
+    return None
